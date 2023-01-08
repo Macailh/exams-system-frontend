c040e5f31c9b74e23b8d9521766adc6bbc2b24f9
Macailh <salvadorenriquegb@gmail.com>
2023-01-08 07:06:28 -0600

    Inject user service

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+
+  public user = {
+    username : '',
+    password : '',
+    name : '',
+    lastname : '',
+    email : '',
+    phone : ''
+  }
+
+  constructor(private userService:UserService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  formSubmit() {
+    console.log(this.user)
+    if(this.user.username == '' || this.user.username == null) {
+      alert('El nombre de usuario es requerido')
+      return;
+    }
+
+    this.userService.addUser(this.user).subscribe(
+      (data) => {
+        console.log(data)
+        alert('Usuario guardado con exito')
+      }, (error) => {
+        console.log(error)
+      }
+    )
+  }
 
 }
