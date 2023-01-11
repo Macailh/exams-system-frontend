1e129fbaff7960f1436d63da98801816398ef492
Macailh <salvadorenriquegb@gmail.com>
2023-01-11 06:56:48 -0600

    Add basic authentication communication

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginData = {
+    "username" : "",
+    "password" : ""
+  }
 
+  constructor(private snack:MatSnackBar, private loginService:LoginService){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  formSubmit() {
+    if(this.loginData.username.trim() == '' || this.loginData.username == null) {
+      this.snack.open('El nombre de usuario es requerido', 'Aceptar',{
+        duration:3000
+      })
+      return
+    }
+
+    if(this.loginData.password.trim() == '' || this.loginData.password == null) {
+      this.snack.open('La contraseña es requerida', 'Aceptar',{
+        duration:3000
+      })
+      return
+    }
+
+    this.loginService.generateToken(this.loginData).subscribe(
+      (data:any) => {
+        console.log(data);
+      }, (error) => {
+        console.log(error);
+      }
+    )
+  }
 }
