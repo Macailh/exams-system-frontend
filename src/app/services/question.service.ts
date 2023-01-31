fbe2a9ea8c70352fbc050a077e88996fe4e4cbef
Macailh <salvadorenriquegb@gmail.com>
2023-01-31 12:17:33 -0600

    Add question service

---
+++ src/app/services/question.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionService {
+
+  constructor(private http: HttpClient) { }
+
+  public getQuestionsFromExam(id: any) {
+    return this.http.get(`${baseUrl}/questions/exam/all/${id}`);
+  }
+
+  public saveQuestion(question: any) {
+    return this.http.post(`${baseUrl}/questions/`, question);
+  }
+
+  public deleteQuestion(id: any) {
+    return this.http.delete(`${baseUrl}/questions/${id}`);
+  }
+
+  public updateQuestion(question: any) {
+    return this.http.put(`${baseUrl}/questions/`, question);
+  }
+
+  public getQuestion(id: any) {
+    return this.http.get(`${baseUrl}/questions/${id}`);
+  }
+
+  public getQuestionFromExamForTest(id: any) {
+    return this.http.get(`${baseUrl}/questions/examen/${id}`);
+  }
+
+  public evaluarExamen(questions: any) {
+    return this.http.post(`${baseUrl}/questions/evaluate-exam`, questions);
+  }
+}
