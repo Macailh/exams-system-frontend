98fe1aaf76f11c2427e1d4b8a265334625907599
Macailh <salvadorenriquegb@gmail.com>
2023-01-31 12:22:42 -0600

    Add component for update question

---
+++ src/app/pages/admin/update-question/update-question.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { QuestionService } from 'src/app/services/question.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-update-question',
+  templateUrl: './update-question.component.html',
+  styleUrls: ['./update-question.component.css']
+})
+export class UpdateQuestionComponent implements OnInit {
+
+  questionId: any
+  question: any
+  exam: any
+
+  constructor(
+    private route: ActivatedRoute,
+    private questionService: QuestionService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.questionId = this.route.snapshot.params['questionId'];
+    console.log(this.questionId)
+    this.questionService.getQuestion(this.questionId).subscribe(
+      (data: any) => {
+        this.question = data;
+        console.log(this.question);
+      },
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
+
+  public updateQuestionData() {
+    this.questionService.updateQuestion(this.question).subscribe(
+      (data) => {
+        Swal.fire('Pregunta actualizada', 'La pregunta ha sido actualizada con éxito', 'success').then((e) => {
+          this.router.navigate(['/admin/ver-preguntas/' + this.question.exam.examId + '/' + this.question.exam.title]);
+        })
+      }
+    )
+  }
+}
