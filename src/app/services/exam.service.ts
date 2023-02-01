3fbae4bf30332bc2145dbf0b5fca4fa005714065
Macailh <salvadorenriquegb@gmail.com>
2023-01-31 22:59:55 -0600

    Fix exam services routes

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -7,38 +7,38 @@ import baseUrl from './helper';
 })
 export class ExamService {
 
-  
-  constructor(private http:HttpClient) { }
 
-  public listExams(){
+  constructor(private http: HttpClient) { }
+
+  public listExams() {
     return this.http.get(`${baseUrl}/exams`);
   }
 
-  public addExam(exam:any){
-    return this.http.post(`${baseUrl}/exams/`,exam);
+  public addExam(exam: any) {
+    return this.http.post(`${baseUrl}/exams/`, exam);
   }
 
-  public deleteExam(id:any){
+  public deleteExam(id: any) {
     return this.http.delete(`${baseUrl}/exams/${id}`);
   }
 
-  public getExam(id:any){
+  public getExam(id: any) {
     return this.http.get(`${baseUrl}/exams/${id}`);
   }
 
-  public updateExam(exam:any){
-    return this.http.put(`${baseUrl}/exams/`,exam);
+  public updateExam(exam: any) {
+    return this.http.put(`${baseUrl}/exams/`, exam);
   }
 
-  public listExamsFromCategory(id:any){
-    return this.http.get(`${baseUrl}/exams/categoria/${id}`);
+  public listExamsFromCategory(id: any) {
+    return this.http.get(`${baseUrl}/exams/category/${id}`);
   }
 
-  public getActiveExams(){
+  public getActiveExams() {
     return this.http.get(`${baseUrl}/exams/active`);
   }
 
-  public getActiveExamsFromCategory(id:any){
-    return this.http.get(`${baseUrl}/exams/categoria/activo/${id}`);
+  public getActiveExamsFromCategory(id: any) {
+    return this.http.get(`${baseUrl}/exams/category/active/${id}`);
   }
 }
